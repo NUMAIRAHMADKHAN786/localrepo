f8688677c79a6db1e291e716479038e44354f4b4
NUMAIRAHMADKHAN786 <numairahmadkhan813@gmail.com>
2025-07-01 23:37:50 +0530

    start of react

---
+++ PracticeConcept/asynccode.js
@@ -0,0 +1,9 @@
+// javascript is asynchronous language and it has event loop which is used to handle asynchronous code 
+// single threaded nature of javascript means that it can only execute one piece of code at a time
+// but it can handle asynchronous code using event loop
+// each operation waits for the previous operation to complete before executing
+// blocking code vs blocking code
+// blocking code is code that stops the execution of the program until it completes
+// non-blocking code is code that allows the program to continue executing while it waits for the operation to complete
+// asynchronous code is code that allows the program to continue executing while it waits for the operation to complete
+// synchronous code is code that executes one line at a time and waits for the previous line to complete before executing the next line
